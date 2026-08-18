acb12eac54a8185086024ab037c9831720343b26
George Zhu <gzhu@ttnfs.com>
2026-08-18 14:37:41 -0500

    add: hello one

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello, world!"));
+app.get("/one", (req, res) => res.send("Hello, one!"));
 
 console.log("test cache");
 
