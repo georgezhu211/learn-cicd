8bc2fc65f1d85f3885e1ac698685cf6ebea06242
George Zhu <gzhu@ttnfs.com>
2026-08-18 14:02:47 -0500

    fix breaking change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Goodbye, world!"));
+app.get("/", (req, res) => res.send("Hello, world!"));
 
 console.log("test cache");
 
